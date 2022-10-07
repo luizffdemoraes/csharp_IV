98d1db4a2c3e1a853361c25d764afa2d819cfbfb
Luiz Felipe Ferreira de Moraes <lffm1994@gmail.com>
2022-10-07 00:08:29 -0300

    Pilha de chamadas

--- ByteBank/Program.cs
+++ ByteBank/Program.cs
@@ -10,9 +10,36 @@ namespace ByteBank
     {
         static void Main(string[] args)
         {
-            ContaCorrente conta = new ContaCorrente(7480, 874250);
-            Console.WriteLine(ContaCorrente.TaxaOperacao);
+            //ContaCorrente conta = new ContaCorrente(7480, 874250);
+            //Console.WriteLine(ContaCorrente.TaxaOperacao);
+
+            Metodo();
+
             Console.ReadLine();
         }
+
+        private static void Metodo()
+        {
+            TestaDivisao(2);
+        }
+
+        private static void TestaDivisao(int divisor)
+        {
+            int resultado = Dividir(10, divisor);
+
+            Console.WriteLine("Resultado da divisão de 10 por" + divisor + " é  " + resultado);
+        }
+
+        private static int Dividir(int numero, int divisor)
+        {
+            return numero / divisor;
+        }
+
+        /*
+        Dividir <-
+        TestaDivisao
+        Metodo
+        Main
+        */
     }
 }
