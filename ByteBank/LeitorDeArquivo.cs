8ab5b9d6aa46ffd7020c018dde1e0afe58ea8099
Luiz Felipe Ferreira de Moraes <lffm1994@gmail.com>
2022-10-12 20:04:28 -0300

    IOException

---
+++ ByteBank/LeitorDeArquivo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace ByteBank
+{
+    public class LeitorDeArquivo
+    {
+        public string Arquivo { get; }
+
+        public LeitorDeArquivo(string arquivo)
+        {
+            Arquivo = arquivo;
+            Console.WriteLine("Abrindo arquivo: " + arquivo);
+        }
+
+        public string LerProximaLinha()
+        {
+            Console.WriteLine("Lendo linha...");
+            throw new IOException();
+            return "Linha do arquivo";
+        }
+
+        public void Fechar()
+        {
+            Console.WriteLine("Fechando arquivo.");
+        }
+    }
+}
--- ByteBank/Program.cs
+++ ByteBank/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,38 +11,58 @@ namespace ByteBank
     {
         static void Main(string[] args)
         {
+            CarregarContas();
+            //Metodo();
+            Console.ReadLine();
+
+            //try
+            //{
+            //    ContaCorrente conta = new ContaCorrente(5265, 52665);
+            //    ContaCorrente conta2 = new ContaCorrente(5245, 52425);
+
+
+            //    conta.Depositar(50);
+            //    Console.WriteLine(conta.Saldo);
+            //    //conta.Sacar(500);
+            //    conta.Transferir(500, conta2);
+            //    Console.WriteLine(conta.Saldo);
+            //}
+            //catch (ArgumentException ex)
+            //{
+            //    Console.WriteLine("Erro no parâmetro: " + ex.ParamName);
+            //    Console.WriteLine("Ocorreu um erro do tipo ArgumentException.");
+            //    Console.WriteLine(ex.StackTrace);
+            //    Console.WriteLine(ex.Message);
+            //}
+            //catch (OperacaoFinanceiraException ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //    Console.WriteLine(ex.StackTrace);
+
+            //    Console.WriteLine("Informações da INNER EXCEPTION (exceção interna):");
+
+            //    Console.WriteLine(ex.InnerException.Message);
+            //    Console.WriteLine(ex.InnerException.StackTrace);
+            //}
+        }
+
+        private static void CarregarContas()
+        {
+            LeitorDeArquivo leitor = new LeitorDeArquivo("contas.txt");
+
             try
             {
-                ContaCorrente conta = new ContaCorrente(5265, 52665);
-                ContaCorrente conta2 = new ContaCorrente(5245, 52425);
-
-
-                conta.Depositar(50);
-                Console.WriteLine(conta.Saldo);
-                //conta.Sacar(500);
-                conta.Transferir(500, conta2);
-                Console.WriteLine(conta.Saldo);
+                leitor.LerProximaLinha();
+                leitor.LerProximaLinha();
+                leitor.LerProximaLinha();
+                
+                leitor.Fechar();
             }
-            catch (ArgumentException ex)
+            catch (IOException ex)
             {
-                Console.WriteLine("Erro no parâmetro: " + ex.ParamName);
-                Console.WriteLine("Ocorreu um erro do tipo ArgumentException.");
-                Console.WriteLine(ex.StackTrace);
-                Console.WriteLine(ex.Message);
+                leitor.Fechar();
+                Console.WriteLine("Exceção do tipo IOException capturada e tratada! ");
             }
-            catch (OperacaoFinanceiraException ex)
-            {
-                Console.WriteLine(ex.Message);
-                Console.WriteLine(ex.StackTrace);
-
-                Console.WriteLine("Informações da INNER EXCEPTION (exceção interna):");
-
-                Console.WriteLine(ex.InnerException.Message);
-                Console.WriteLine(ex.InnerException.StackTrace);
-            }
-
-            Metodo();
-            Console.ReadLine();
         }
 
         private static void Metodo()
